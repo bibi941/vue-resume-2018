de4c1e415c7db5efdc57404d1d0e871876c64205
finley <sssrrr@vip.qq.com>
2018-06-06 22:08:09 +0800

    :sparkles: 添加粒子背景

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import TopHeader from './components/TopHeader.vue'
+import ParticlesJs from './components/ParticlesJS.vue'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  components: { TopHeader, ParticlesJs }
 })
