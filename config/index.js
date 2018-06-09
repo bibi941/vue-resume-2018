e9da2095a768162d308ec1b6e0af71e0638b256d
finley <sssrrr@vip.qq.com>
2018-06-10 00:46:24 +0800

    :hammer: 打包

--- config/index.js
+++ config/index.js
@@ -43,7 +43,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-resume-2018/dis/',
+
 
     /**
      * Source Maps
