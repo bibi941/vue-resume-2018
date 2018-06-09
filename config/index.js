27792dbf2a31451432f9b13bf55ec5d5fddc76b9
finley <sssrrr@vip.qq.com>
2018-06-10 00:53:57 +0800

    :bug: 404

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-resume-2018/dis/',
+    assetsPublicPath: '/vue-resume-2018/dist/',
 
 
     /**
